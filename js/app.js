22a5839c7326ad587996a7bb409b5eabf220c419
Polina <neposed222@gmail.com>
2024-02-21 23:03:09 +0200

    commit grid

--- js/app.js
+++ js/app.js
@@ -1,22 +1,4 @@
 let otherWindow = window.open();
 
-// let counter = 0;
-// function btnClick(el) {
-//     counter++;
-//     el.innerHTML = "count: " + counter;
-//     el.class = "new word";
-//     console.log(el.class);
-// }
-
-// function userInput(el) {
-//     if (el.value == "font")
-//         el.style.fontSize = "40px";
-
-//     else if(el.value == "color")
-//         el.style.color = "green";
-//     else if(el.value == "bg")
-//         el.style.backgroundColor = "black";
-// }
-
 
 
